3a10c4268137738926c2ef1c1a5743f7f4986c51
Joel Pérez Miralles <joooel92@hotmail.com>
2023-02-25 10:06:26 +0100

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hola Joel')
+const express = require('express')
+
+// Crear la app
+
+const app = express()
+
+// Definir un puerto y arrancar el proyecto
+
+const port = 3000;
+app.listen(port,  () => {
+    console.log(`El servidor está funcionando en el puerto ${port}`)
+});
